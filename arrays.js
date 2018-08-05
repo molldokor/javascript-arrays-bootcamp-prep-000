201cd31b9640433ec184442f67efd5bd9413af7a
Kevin O'Reilly <molldokor@gmail.com>
2018-08-05 15:36:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,2 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, element) {
-  var newArray = [element, ...array];
-  return array & newArray;
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
-}
