8138760943f5c6812ae0b8f6c954be7410a118d6
Kevin O'Reilly <molldokor@gmail.com>
2018-08-05 16:28:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   var newArray = array.slice(0, array.length - 1);
-  return array;
+  return newArray;
 }
